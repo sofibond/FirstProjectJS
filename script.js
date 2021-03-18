5181a5efb9a7fb4900d492abb0c62b60f9872410
sofibond <sofibond28@gmail.com>
2021-03-18 17:07:53 +0300

    Сделала урок 10

--- script.js
+++ script.js
@@ -7,8 +7,25 @@ const advertisement = document.querySelector('.adv'),
 
 
 advertisement.remove();
+
 booksParent.prepend(book[1]);
 booksParent.append(book[2]);
 book[3].before(book[4]);
-//bodyElem.setAttribute('style', 'background-image: url(./image/adv.jpg)');
+
+bodyElem.setAttribute('style', 'background-image: url(./image/adv.jpg)');
+
+let bookTitle = book[4].querySelector('a');
+bookTitle.textContent = 'Книга 3. this и Прототипы Объектов';
+
+let secondBookElem = book[0].getElementsByTagName('li');
+secondBookElem[3].after(secondBookElem[6]);
+secondBookElem[4].after(secondBookElem[8]);
+let fifthBookElem = book[5].getElementsByTagName('li');
+fifthBookElem[1].after(fifthBookElem[9]);
+fifthBookElem[9].after(fifthBookElem[3]);
+fifthBookElem[4].after(fifthBookElem[9]);
+fifthBookElem[8].after(fifthBookElem[6]);
+
 newElem.textContent = "Глава 8: За пределами ES6";
+let sixthBookElem = book[2].getElementsByTagName('li');
+sixthBookElem[8].after(newElem);
